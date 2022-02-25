ee8cfce2d9197f80393c1db9061669c38ef09fb4
Frank Tang <roadtang@gmail.com>
2022-02-25 23:47:27 +0800

    config  - files -   support ~ like expansion, custom list ui

--- config/config-files.el
+++ config/config-files.el
@@ -2,7 +2,10 @@
 ;;
 ;;  make sure file under the 'find-file-read-only-roots directory is opened in read only mode.
 ;;
-(defcustom find-file-readonly-roots () "files under these directories are opened readonly")
+(defcustom find-file-readonly-roots () 
+  "files under these directories are opened readonly"
+  :type '(repeat directory)
+  )
 
 (defun any (l)
   (if (eq l ()) nil
@@ -11,7 +14,7 @@
 
 (defun read-only-ff-hook ()
   (let ((fn (buffer-file-name (current-buffer))))
-     (if (any (mapcar (lambda (x) (dired-in-this-tree fn  x)) find-file-readonly-roots))
+     (if (any (mapcar (lambda (x) (dired-in-this-tree fn  (expand-file-name x))) find-file-readonly-roots))
          (read-only-mode))
     ))
 
